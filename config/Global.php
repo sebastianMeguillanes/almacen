bcee6ee08acab6d01c686b2f92e1c0e9d3258d3e
sebastianMeguillanes <sebastian.meguillanes@ucb.edu.bo>
2023-11-19 19:47:37 -0400

    sebasq

--- config/Global.php
+++ config/Global.php
@@ -3,7 +3,7 @@
 // Datos de la base de datos
 define("DB_HOST", "181.188.156.195");
 define("DB_NAME", "almacenUCB1");
-define("DB_USERNAME", "superadmin");
+define("DB_USERNAME", "admin");
 define("DB_PASSWORD", "admin1234");
 define("DB_PORT", "18010");
 define("DB_ENCODE", "utf8");
